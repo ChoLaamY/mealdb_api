4e8dd4ce7c25fc463121cf5b4f509ef26b962fef
ChoLaamY <cy2617@columbia.edu>
2022-12-16 09:14:32 -0500

    updated the tests

--- tests/test_mealdb_api.py
+++ tests/test_mealdb_api.py
@@ -1,16 +1,16 @@
 from mealdb_api import mealdb_api
 import pytest
-from mealdb_api import list_all, define_ingredient, filter_recipes, recipe_ingredients
+# from mealdb_api import list_all, define_ingredient, filter_recipes, recipe_ingredients
 
 # test functions for list_all
 def test_list_all_assert():
     with pytest.raises(Exception):
-        list_all('hello')
+        mealdb_api.list_all('hello')
 
 # test functions for define_ingredient
 def test_define_ingredient_assert():
     with pytest.raises(Exception):
-        define_ingredient(5)
+        mealdb_api.define_ingredient(5)
 
 @pytest.mark.parametrize('example_def, expected_def', [
     ('milk', 'No definition avaliable'),
@@ -20,14 +20,14 @@ def test_define_ingredient_assert():
 ])
 
 def test_define_ingredient(example_def, expected_def):
-    define = define_ingredient(example_def)
+    define = mealdb_api.define_ingredient(example_def)
     key = list(define.keys())
     assert expected_def == define[key[0]]
 
 # test functions for filter_recipes
 def test_filter_recipe_assertion():
     with pytest.raises(AssertionError):
-        filter_recipes('table')
+        mealdb_api.filter_recipes('table')
 
 @pytest.mark.parametrize('example_fil, expected_fil', [
     ('blackberries', ['Fruit and Cream Cheese Breakfast Pastries', 'Summer Pudding']),
@@ -36,12 +36,12 @@ def test_filter_recipe_assertion():
 ])
 
 def test_filter_recipes(example_fil, expected_fil):
-    assert expected_fil == filter_recipes(example_fil)
+    assert expected_fil == mealdb_api.filter_recipes(example_fil)
 
 # test functions for recipe_ingredients
 def test_recipe_ingredients_assert():
     with pytest.raises(AssertionError):
-        recipe_ingredients(900)
+        mealdb_api.recipe_ingredients(900)
 
 @pytest.mark.parametrize('example_rec, expected_rec', [
     ('Pizza Express Margherita', 11),
@@ -52,5 +52,5 @@ def test_recipe_ingredients_assert():
 ])
 
 def test_recipe_ingredients(example_rec, expected_rec):
-    df = recipe_ingredients(example_rec)[0]
+    df = mealdb_api.recipe_ingredients(example_rec)[0]
     assert expected_rec == len(df)
